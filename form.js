170cf3a56d3b9e590fd9e7874ca46a805e0a5923
SyedDanyal190 <124075426+SyedDanyal190@users.noreply.github.com>
2023-02-25 00:25:03 +0500

    Add files via upload

---
+++ form.js
@@ -0,0 +1,11 @@
+function validateForm()
+{
+    Let va = document.getelementbyId("name").value;
+    if(va =='')
+    {
+        alert["Please enter  value"]
+    }
+    elseif(va.length<3)&&(va.length>7)
+    alert('');
+    return false
+}
